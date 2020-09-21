baa4d488e45f8beb8efc7f477979fab0450db567
Carson Lampila <43243165+CarsonLampila@users.noreply.github.com>
2020-09-21 14:26:46 -0400

    Finished E06.5

---
+++ E06.5/AppClass.cpp
@@ -0,0 +1,144 @@
+#include "AppClass.h"
+void Application::InitVariables(void)
+{
+	//Change this to your name and email
+	m_sProgrammer = "Alberto Bobadilla - labigm@rit.edu";
+
+	//Set the position and target of the camera
+	m_pCameraMngr->SetPositionTargetAndUpward(vector3(5.0f,3.0f,15.0f), ZERO_V3, AXIS_Y);
+
+	m_pModel = new Simplex::Model();
+	m_pModel->Load("Sorted\\WallEye.bto");
+	
+	m_stopsList.push_back(vector3(-4.0f, -2.0f, 5.0f));
+	m_stopsList.push_back(vector3(1.0f, -2.0f, 5.0f));
+
+	m_stopsList.push_back(vector3(-3.0f, -1.0f, 3.0f));
+	m_stopsList.push_back(vector3(2.0f, -1.0f, 3.0f));
+
+	m_stopsList.push_back(vector3(-2.0f, 0.0f, 0.0f));
+	m_stopsList.push_back(vector3(3.0f, 0.0f, 0.0f));
+
+	m_stopsList.push_back(vector3(-1.0f, 1.0f, -3.0f));
+	m_stopsList.push_back(vector3(4.0f, 1.0f, -3.0f));
+
+	m_stopsList.push_back(vector3(0.0f, 2.0f, -5.0f));
+	m_stopsList.push_back(vector3(5.0f, 2.0f, -5.0f));
+
+	m_stopsList.push_back(vector3(1.0f, 3.0f, -5.0f));
+}
+void Application::Update(void)
+{
+	//Update the system so it knows how much time has passed since the last call
+	m_pSystem->Update();
+
+	//Is the arcball active?
+	ArcBall();
+
+	//Is the first person camera active?
+	CameraRotation();
+}
+void Application::Display(void)
+{
+	// Clear the screen
+	ClearScreen();
+
+	// Draw the model
+	m_pModel->PlaySequence();
+
+	//Get a timer
+	static float fTimer = 0;	//store the new timer
+	static uint uClock = m_pSystem->GenClock(); //generate a new clock for that timer
+	fTimer += m_pSystem->GetDeltaTime(uClock); //get the delta time for that timer
+
+	//calculate the current position
+	vector3 v3CurrentPos;
+	
+
+
+
+	//your code goes here
+
+	// Define position variables
+	vector3 current;
+	vector3 next;
+	static uint stopNum = 0;
+	int destination = stopNum + 1;
+
+	// Define Current
+	current = m_stopsList[stopNum];
+	// Define Next as following stop and loop back to start
+	if (destination >= m_stopsList.size())
+	{
+		destination = 0;
+	}
+	next = m_stopsList[destination];
+
+	// Define times
+	float arriveTime;
+	// Regular
+	if (stopNum != m_stopsList.size() - 1)
+	{
+		arriveTime = 3.0;
+	}
+	// End - Start
+	else
+	{
+		arriveTime = 9.0;
+	}
+	float timePercent = MapValue(fTimer, 0.0f, arriveTime, 0.0f, 1.0f);
+	
+	// Calculate the current pos 
+	v3CurrentPos = glm::lerp(current, next, timePercent);
+
+	// If arrived at next stop
+	if (timePercent >= 1.0f)
+	{
+		// Alternate 
+		stopNum++;
+		// Restart time
+		fTimer = m_pSystem->GetDeltaTime(uClock);
+		// Reset stopNum at end
+		if (stopNum >= m_stopsList.size())
+		{
+			stopNum = 0;
+		}
+	}
+	//-------------------
+	
+
+
+
+	matrix4 m4Model = glm::translate(v3CurrentPos);
+	m_pModel->SetModelMatrix(m4Model);
+
+	m_pMeshMngr->Print("\nTimer: ");//Add a line on top
+	m_pMeshMngr->PrintLine(std::to_string(fTimer), C_YELLOW);
+
+	// Draw stops
+	for (uint i = 0; i < m_stopsList.size(); ++i)
+	{
+		m_pMeshMngr->AddSphereToRenderList(glm::translate(m_stopsList[i]) * glm::scale(vector3(0.05f)), C_GREEN, RENDER_WIRE);
+	}
+	
+	// draw a skybox
+	m_pMeshMngr->AddSkyboxToRenderList();
+	
+	//render list call
+	m_uRenderCallCount = m_pMeshMngr->Render();
+
+	//clear the render list
+	m_pMeshMngr->ClearRenderList();
+	
+	//draw gui
+	DrawGUI();
+	
+	//end the current frame (internally swaps the front and back buffers)
+	m_pWindow->display();
+}
+void Application::Release(void)
+{
+	SafeDelete(m_pModel);
+	//release GUI
+	ShutdownGUI();
+}
